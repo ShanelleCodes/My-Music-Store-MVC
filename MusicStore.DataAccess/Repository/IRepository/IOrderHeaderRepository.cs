3bd1b98c2d8c405b64b88e428478281f6c647fce
Shanelle Codes <shanelle.codes@gmail.com>
2024-02-14 08:59:45 -0500

    first commit
    upload music store file

---
+++ MusicStore.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -0,0 +1,14 @@
+﻿using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.DataAccess.Repository.IRepository
+{
+    public interface IOrderHeaderRepository : IRepository<OrderHeader>
+    {
+        void Update(OrderHeader obj);
+    }
+}
---
+++ MusicStore.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using MusicStore.DataAccess.Data;
+using MusicStore.DataAccess.Repository.IRepository;
+using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+           var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if(objFromDb != null)
+            {
+                objFromDb.SongName = obj.SongName;
+                objFromDb.Artist = obj.Artist;
+                objFromDb.Album = obj.Album;
+                //objFromDb.Genre = obj.Genre;
+                objFromDb.Price = obj.Price;
+                if(obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
+    }
+}
